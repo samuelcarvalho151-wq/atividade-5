4791baf61879aad5b2d6ae6d4034955b9a9b3b4e
SAMUEL DOS SANTOS CARVALHO <samuel_carvalho151@estudante.sesisenai.org.br>
2026-08-24 15:35:01 -0300

    js04

--- script.js
+++ script.js
@@ -63,3 +63,19 @@
 //    - Limpe os campos depois: campo.value = ''
 //
 // Comece pelo exercicio 1 e va ate o 4. Boa sorte!
+
+    document.getElementById('form-contato').addEventListener('submit', function(evento) {
+        evento.preventDefault();
+        const nome = document.getElementById('nome').value;
+        const email = document.getElementById('email').value;
+        const mensagem = document.getElementById('mensagem').value;
+
+        if (nome === '' || email === '' || mensagem === '') {
+            alert('Preencha todos os campos!');
+        } else {
+            document.getElementById('resposta-form').textContent = `Obrigado, ${nome}! Sua mensagem foi enviada.`;
+            document.getElementById('nome').value = '';
+            document.getElementById('email').value = '';
+            document.getElementById('mensagem').value = '';
+        }
+    });
